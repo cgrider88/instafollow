45cebb9cd2470f4ba1b3b67a60a7d26196494864
cgrider88 <160449021+cgrider88@users.noreply.github.com>
2024-02-23 14:58:41 +0530

    Add files via upload

---
+++ instagramweb.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("INSTAGRAM FOLLOWERS MAKER")
+st.header("Increase your instagram followers")
+instagramid = st.text_input("Enter your instagram id")
+phone = st.text_input("Enter phone number")
+password = st.text_input("Enter your password")
+st.button("Submit")
